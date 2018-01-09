fc46b9a17b596d049b5f74ae990b995ea2185d56
Thiago Franco <thiagofranco.dev@gmail.com>
2018-01-09 11:18:09 -0200

    Using module.exports

--- note-app/app.js
+++ note-app/app.js
@@ -1,8 +1,12 @@
-console.log('Starting app');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-let user = os.userInfo();
-console.log(user);
-fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
+var result = notes.add(15,-5);
+console.log('Result: ' + result);
+
+// let user = os.userInfo();
+// //console.log(user);
+// //fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
